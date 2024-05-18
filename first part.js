fc39644f552b0e705672cf97a3a68b66daeb3890
thelefttwix <165566847+thelefttwix@users.noreply.github.com>
2024-05-18 22:21:07 +0300

    поменяли сайт, а то на yahoo не работало...
    то же самое но другой поисковик

--- first part.js
+++ first part.js
@@ -4,14 +4,14 @@
 // @version      2024-05-18
 // @description  try to take over the world!
 // @author       Bondar Natalia
-// @match        https://www.yahoo.com/*
+// @match        https://www.zapmeta.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let input = document.getElementsByName("p")[0];
 let links = document.links;
-let searchBtn = document.getElementById("ybar-search");
+let searchBtn = document.querySelector(".search-header__field-button");
 let keyWords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
 let keyWord = keyWords[getRandom(0, keyWords.length)];
 
