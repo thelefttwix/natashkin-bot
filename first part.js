3a4e5918cf4280cbbc6174c59b0a036b52d2277b
thelefttwix <165566847+thelefttwix@users.noreply.github.com>
2024-05-18 20:06:30 +0300

    First part of my yahoo bot)
    Understands where the search results and the main page are located. Knows how to find a site in search results and go to it. Generates random key phrases.

---
+++ first part.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         My new bot
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-18
+// @description  try to take over the world!
+// @author       Bondar Natalia
+// @match        https://www.yahoo.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+let input = document.getElementsByName("p")[0];
+let links = document.links;
+let searchBtn = document.getElementById("ybar-search");
+let keyWords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
+let keyWord = keyWords[getRandom(0, keyWords.length)];
+
+if(searchBtn !== null) {
+    input.value = keyWord;
+    searchBtn.click();
+} else {
+    for (let i = 0; i < links.length; i++) {
+        if(links[i].href.indexOf("napli.ru") != -1) {
+            let link = links[i];
+            console.log("Нашел строку" + links[i]);
+            link.click();
+            break;
+        }
+    }
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
